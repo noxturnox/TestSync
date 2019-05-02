7f1fb57c36aa99e35f3edd2e8fed59e015e4351c
Ricardo <ricardo@treexor.com>
2019-05-02 13:48:04 +0100

    First Commit Branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -306,12 +306,19 @@ module.exports = function (grunt) {
                     stdout: false,
                 }
             },
+            createDevelopBranch: {
+                command: 'git branch develop',
+            },
             syncFirstTime: {
                 command: (shopName,env)=>[`cd stores/${shopName}`,`theme download --env=${env}`].join(' && '), //
             },
-            cleanFirstTime: {
-                command: ['rm stores/**/config.yml','git add .','git commit -a -m "cleaningFirstTime"'
-                ,'git tag v0.1.0','git checkout -b develop','git tag pre-v0.1.0','git push origin master develop : --tags'].join(' && '),
+            branchMastercleanFirstTime: {
+                command: ['rm stores/**/config.yml','git add .','git commit -a -m "First Commit Branch"'
+                ,'git tag v0.1.0','git checkout develop'].join(' && '),
+            },
+            branchDevelopcleanFirstTime: {
+                command: ['rm stores/**/config.yml','git add .','git commit -a -m "First Commit Branch"'
+                ,'git tag pre-v0.1.0','git push origin master develop : --tags'].join(' && '),
             },
         }
     })
@@ -556,21 +563,29 @@ module.exports = function (grunt) {
     })
     grunt.registerTask('FirstTimeSync',function(){
         var tempVarYML = grunt.file.readYAML('config.yml');
-        var arrayTasks = [];
+        var arrayTasksBranchMaster = [];
+        var arrayTasksBranchDevelop = [];
+        var arrayStores = [];
         var arrayTasksFolder = [];
-        for (const shopName in tempVarYML) {
-            arrayTasksFolder.push('shell:createFolder:'+shopName)
-            if(shopName=='common-files') {
-                continue;
+            for (const shopName in tempVarYML) {
+                arrayTasksFolder.push('shell:createFolder:'+shopName)
+                if(shopName=='common-files') {
+                  continue;
+                }
+                arrayStores.push(shopName);
+                for (const envName in tempVarYML[shopName]){
+                    if(envName == 'master')
+                        arrayTasksBranchMaster.push('shell:syncFirstTime:'+shopName+':'+envName);
+                    else
+                        arrayTasksBranchDevelop.push('shell:syncFirstTime:'+shopName+':'+envName);
+                }
             }
-            for (const envName in tempVarYML[shopName]){
-                if(envName=='develop')
-                arrayTasks.push('shell:syncFirstTime:'+shopName+':'+envName);
-            }
-        }
-        grunt.task.run(arrayTasksFolder)
-        .run('createYAMLFileOnEachShop')
-        .run(arrayTasks)
-        .run('shell:cleanFirstTime'); //
+            grunt.task.run(arrayTasksFolder)
+            .run('createYAMLFileOnEachShop')
+            .run('shell:createDevelopBranch')
+            .run(arrayTasksBranchMaster)
+            .run('shell:branchMastercleanFirstTime')
+            .run(arrayTasksBranchDevelop)
+            .run('shell:branchDevelopcleanFirstTime')
     })
 }
